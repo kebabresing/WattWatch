8e7e7f81f4b545cc46ca7219f50912d27d9b43fa
kebabresing <kebabresing@users.noreply.github.com>
2026-08-20 09:45:00 +0700

    Expose config.js and remove unused build scripts

---
+++ config.js
@@ -0,0 +1,6 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCsmVfIm1eTLsuU7s1XlklRTdLUzdovDI4",
+  authDomain: "monitoring-oled.firebaseapp.com.firebaseapp.com",
+  databaseURL: "https://monitoring-oled-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "monitoring-oled",
+};
